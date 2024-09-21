ee5b99c530d5ed84e7e23d497e6663fcf95ec27b
Jason Mansfield <jason@lub-dub.org>
2024-09-20 18:22:08 -0700

    help link

--- twitch.go
+++ twitch.go
@@ -46,6 +46,11 @@ func init() {
 		Description: "Integration with Twitch APIs",
 		WebPaths: []*modules.ManifestWebPath{
 			{
+				Path:        "https://autonomouskoi.org/mod-twitch.html",
+				Type:        modules.ManifestWebPathType_MANIFEST_WEB_PATH_TYPE_HELP,
+				Description: "Help!",
+			},
+			{
 				Path:        "/m/twitch/",
 				Type:        modules.ManifestWebPathType_MANIFEST_WEB_PATH_TYPE_EMBED_CONTROL,
 				Description: "Controls for Twitch",
